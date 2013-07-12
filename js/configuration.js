c365ed6b0a12304751009a5395132a7fbb27ed66
Pablo Moncada Isla <pmoncada@krusty.gsi.dit.upm.es>
2013-07-12 09:52:46 +0200

    Inicial commit

---
+++ js/configuration.js
@@ -0,0 +1,7 @@
+endPoint = 'http://krusti.gsi.dit.upm.es:8080/LMF-2.6.0/';
+
+endPointSOLR = 'http://krusti.gsi.dit.upm.es:8080/LMF-2.6.0/solr/companies/select?json.nl=map&wt=json&rows=1000';
+
+endPointJSON = "http://krusti.gsi.dit.upm.es:8080/LMF-2.6.0/config/data/";
+
+endPointSemanticMatcher = "http://lab.gsi.dit.upm.es/~pmoncada/episteme/mockup.php";
